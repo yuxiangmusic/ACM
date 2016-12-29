8fbc9d0755d1ce14ed7f1400bf858ef104ea9555
Yuxiang Zhang <yuxiangzhang@ensoftcorp.com>
2016-12-28 19:16:53 -0600

    Improve subsets efficiency

--- src/notebook/set/Subsets.java
+++ src/notebook/set/Subsets.java
@@ -2,14 +2,12 @@ package notebook.set;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Subsets {
 
 	public static void main(String[] args) {
-		System.out.println(subsets(new int[] { 1, 2, 3, 4 }));
+		System.out.println(subsets(new int[] { 0, 1, 2, 3 }));
 		System.out.println(subsets(new int[] { 1, 1, 2, 2 }));
 		System.out.println(subsetsWithDup(new int[] { 1, 1, 2, 2 }));
 	}
@@ -29,16 +27,16 @@ public class Subsets {
 
 	public static List<List<Integer>> subsetsWithDup(int[] nums) {
 		Arrays.sort(nums);
-		Set<List<Integer>> set = new LinkedHashSet<>();
-		set.add(new ArrayList<>());
-		for (int i = 0; i < nums.length; i++) {
-			for (List<Integer> l : new ArrayList<>(set)) {
-				List<Integer> copy = new ArrayList<>(l);
-				copy.add(nums[i]);
-				set.add(copy);
+		List<List<Integer>> list = new ArrayList<>();
+		list.add(new ArrayList<>());
+		for (int i = 0, begin = 0, size = list.size(); i < nums.length; i++, begin = size, size = list.size()) {
+			for (int j = i > 0 && nums[i] == nums[i - 1] ? begin : 0; j < size; j++) {
+				List<Integer> l = new ArrayList<>(list.get(j));
+				l.add(nums[i]);
+				list.add(l);
 			}
 		}
-		return new ArrayList<>(set);
+		return list;
 	}
 
 }

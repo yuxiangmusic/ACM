8d410cae936a882de6ffbe1c8dd4b637e7c17821
Yuxiang Zhang <yuxiangzhang@ensoftcorp.com>
2017-03-27 10:39:10 -0500

    Update bias binary search

--- src/notebook/search/SearchUtil.java
+++ src/notebook/search/SearchUtil.java
@@ -1,49 +1,54 @@
 package notebook.search;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class SearchUtil {
 
-	public static void main(String[] args) {
+	@Test
+	public void test() {
 		int[] arr = new int[] { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 };
 
-		for (int i = 1; i <= 5; i++) {
-			System.out.println(i + " index from " + binarySearchBiasL(arr, i) + " to " + binarySearchBiasR(arr, i));
-		}
+		assertEquals(0, binarySearchBiasL(arr, 1));
+		assertEquals(0, binarySearchBiasR(arr, 1));
+
+		assertEquals(1, binarySearchBiasL(arr, 2));
+		assertEquals(2, binarySearchBiasR(arr, 2));
+
+		assertEquals(3, binarySearchBiasL(arr, 3));
+		assertEquals(5, binarySearchBiasR(arr, 3));
+
+		assertEquals(6, binarySearchBiasL(arr, 4));
+		assertEquals(9, binarySearchBiasR(arr, 4));
+
+		assertEquals(10, binarySearchBiasL(arr, 5));
+		assertEquals(14, binarySearchBiasR(arr, 5));
 	}
 
 	public static int binarySearchBiasL(int[] arr, int key) {
-		int l = 0, h = arr.length - 1;
-		while (l <= h) {
+		int l = -1, h = arr.length - 1;
+		while (l < h - 1) {
 			int m = (l + h) >>> 1;
-			if (key == arr[m]) {
-				if (m == 0 || arr[m - 1] < arr[m])
-					return m;
-				else
-					h = m - 1;
-			} else if (key < arr[m]) {
-				h = m - 1;
-			} else {
-				l = m + 1;
-			}
+			if (arr[m] >= key)
+				h = m;
+			else
+				l = m;
 		}
-		return -1 - l;
+		return h;
+
 	}
 
 	public static int binarySearchBiasR(int[] arr, int key) {
-		int l = 0, h = arr.length - 1;
-		while (l <= h) {
+		int l = 0, h = arr.length;
+		while (l < h - 1) {
 			int m = (l + h) >>> 1;
-			if (key == arr[m]) {
-				if (m == arr.length - 1 || arr[m + 1] > arr[m])
-					return m;
-				else
-					l = m + 1;
-			} else if (key < arr[m]) {
-				h = m - 1;
-			} else {
-				l = m + 1;
-			}
+			if (arr[m] <= key)
+				l = m;
+			else
+				h = m;
 		}
-		return -1 - l;
+		return l;
 	}
 
 	/**
@@ -52,7 +57,7 @@ public class SearchUtil {
 	public static int binarySearch(int[] arr, int key) {
 		int l = 0, h = arr.length - 1;
 		while (l <= h) {
-			int m = (l + h) >>> 1; // unsigned bit shift to avoid overflow
+			int m = (l + h) >>> 1;
 			if (key == arr[m])
 				return m;
 			else if (key < arr[m])
@@ -69,7 +74,7 @@ public class SearchUtil {
 
 	private static int binarySearchRec(int[] arr, int key, int l, int h) {
 		if (l > h)
-			return -1 - l;
+			return ~l;
 		int m = (l + h) >>> 1;
 		if (key == arr[m])
 			return m;
--- src/notebook/set/Subsets.java
+++ src/notebook/set/Subsets.java
@@ -8,29 +8,30 @@ public class Subsets {
 
 	public static void main(String[] args) {
 		System.out.println(subsets(0, 1, 2, 3));
-		System.out.println(subsets(0, 1, 2, 1));
-		System.out.println(subsetsWithDup(0, 1, 2, 1));
+		System.out.println(subsets(0, 0, 1, 1));
+		System.out.println(subsetsWithDup(0, 0, 1, 1));
 	}
 
 	public static List<List<Integer>> subsets(int... nums) {
-		List<List<Integer>> list = new ArrayList<>();
-		list.add(new ArrayList<>());
-		for (int i = 0, size = list.size(); i < nums.length; i++, size = list.size()) {
+		List<List<Integer>> subsets = new ArrayList<>(Arrays.asList(new ArrayList<>()));
+		for (int i = 0, size = subsets.size(); i < nums.length; i++, size = subsets.size()) {
 			for (int j = 0; j < size; j++) {
-				List<Integer> l = new ArrayList<>(list.get(j));
+				List<Integer> l = new ArrayList<>(subsets.get(j));
 				l.add(nums[i]);
-				list.add(l);
+				subsets.add(l);
 			}
 		}
-		return list;
+		return subsets;
 	}
 
+	/**
+	 * The given set may contain duplicates, but the solution must not contain duplicates
+	 */
 	public static List<List<Integer>> subsetsWithDup(int... nums) {
 		Arrays.sort(nums);
-		List<List<Integer>> subsets = new ArrayList<>();
-		subsets.add(new ArrayList<>());
+		List<List<Integer>> subsets = new ArrayList<>(Arrays.asList(new ArrayList<>()));
 		for (int i = 0, presize = 0, cursize = subsets.size(); i < nums.length; i++, presize = cursize, cursize = subsets.size()) {
-			for (int j = i > 0 && nums[i] == nums[i - 1] ? presize : 0; j < cursize; j++) {
+			for (int j = i == 0 || nums[i] != nums[i - 1] ? 0 : presize; j < cursize; j++) {
 				List<Integer> subset = new ArrayList<>(subsets.get(j));
 				subset.add(nums[i]);
 				subsets.add(subset);
